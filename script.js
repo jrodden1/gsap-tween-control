7c9142f9e0db218ae2511243e7323bae899f8453
Jeremiah Rodden <jrodden1@gmail.com>
2020-01-15 11:20:46 -0700

    Learned about tween controls, starting tween in paused state; added notes

--- script.js
+++ script.js
@@ -7,10 +7,18 @@ document.addEventListener('click', function (event) {
 })
 
 
-var tween = gsap.to(".green", {duration:3, x:600, ease:"linear", paused:true});
+// Initial Animation Test
+// gsap.to(".green", {duration:3, x:600, ease:"linear"})
 
+// Now we need to save this into a variable so I can call stuff on it.
+let tween = gsap.to(".green", {duration:3, x:600, ease:"linear", paused:true});
+// used the "paused" property to make it so the animation doesn't automatically start until you want it to (by calling the .play() on the tween)
 
+// This is selecting the buttons that are over in index.html
 document.getElementById("play").onclick = ()=> tween.play();
 document.getElementById("pause").onclick = ()=> tween.pause();
 document.getElementById("reverse").onclick = ()=> tween.reverse();
 document.getElementById("restart").onclick = ()=> tween.restart();
+
+// Above notice the 4 basic methods for running the tween:
+// play, pause, reverse, restart
